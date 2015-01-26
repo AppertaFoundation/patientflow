11714bffc253f39024f7b68362043ddf81e4b60c
Colin Wren <colin@neovahealth.co.uk>
2015-01-26 13:41:58 +0000

    Adding KSS based styleguide for Joe to work from

---
+++ nh_etake_list/static/dev/less/test/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# compile the CSS file
+lessc ../src/etake_style.less > css/etake_style.css
+
+# lint over the files
+csslint css/etake_style.css
+
+# generate the styleguide
+python generate_styleguide.py
+
+# use UnCSS to ensure that all styles are used
+uncss styleguide.html > ../../../src/css/etake_style.css
+diff --suppress-common-lines -i -E -Z -b -B -w css/etake_style.css ../../../src/css/etake_style.css
+
+echo "## Minifying UnCSS'd file"
+lessc ../../../src/css/etake_style.css ../../../src/css/etake_style.css -x
+
+filesize="$(du -h ../../../src/css/etake_style.css | cut -f1)"
+echo "Resulting file is ${filesize}B"
