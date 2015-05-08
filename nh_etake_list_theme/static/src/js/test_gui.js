4f2b303099087fcc4c1612b61c5e4353cdd7787e
amipatel <amipatel@neovahealth.co.uk>
2015-05-08 15:57:52 +0100

    [IMP][WI-548] p-flow GUI Testing using tours

--- nh_etake_list_theme/static/src/js/test_gui.js
+++ nh_etake_list_theme/static/src/js/test_gui.js
@@ -5,7 +5,7 @@
 
 
     openerp.Tour.register({
-        id: 'referral_nurse_able_to_create_referral',
+        id: 'referral_nurse_able_to_update_patient_arrival',
         name: _t("Reconcile the demo bank statement"),
         path: '/web?debug=',
         //mode: 'test',
@@ -19,39 +19,14 @@
             },
 
             {
-                title:     _t("Referral form"),
+                title:     _t("Referral Board"),
                 element:   '.oe_menu_text:contains("Referral Forms")'
             },
 
              {
-                title:     _t("Create Referral"),
-                element:   '.oe_button.oe_list_add.oe_highlight:contains("Create")'
+                title:     _t("Update patient arrival"),
+                element:   'td.oe_kanban_column:nth-child(2) .oe_fold_column.oe_kanban_record:first() button'
             },
-
-            {
-                title:     _t("Select Patient"),
-                element:   '#oe-field-input-16',
-                sampleText: 'Klocko, Lindell'
-            },
-            {
-                title:     _t("Enter Symptoms"),
-                element:   'textarea[name=symptoms_notes]',
-                sampleText: 'Test Symptom Notes'
-            },
-            {
-                title:     _t("Enter Medical History"),
-                element:   'textarea[name=medical_history_notes]',
-                sampleText: 'Test Medical History Notes'
-            },
-            {
-                title:     _t("Enter Allergies"),
-                element:   'textarea[name=allergies]',
-                sampleText: 'Test Allergies'
-            },
-            {
-                title:     _t("Save"),
-                element:   '.oe_form_button_save'
-            }
 
         ]
     });
--- nh_etake_list_theme/tests/test_referral_form_ui.py
+++ nh_etake_list_theme/tests/test_referral_form_ui.py
@@ -6,6 +6,6 @@ class TestReferralFormUI(openerp.tests.HttpCase):
 
     def test_referral_form_creation(self):
         self.phantom_js("/web",
-            "openerp.Tour.run('referral_nurse_able_to_create_referral', 'test')",
-            "openerp.Tour.tours.referral_nurse_able_to_create_referral", login="taylor"
+            "openerp.Tour.run('referral_nurse_able_to_update_patient_arrival', 'test')",
+            "openerp.Tour.tours.referral_nurse_able_to_update_patient_arrival", login="taylor"
         )
