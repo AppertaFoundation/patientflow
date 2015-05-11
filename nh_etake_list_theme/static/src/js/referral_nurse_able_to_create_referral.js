3e89a5dad7c104c42609eed7578892fe12e22281
amipatel <amipatel@neovahealth.co.uk>
2015-05-11 11:18:44 +0100

    [IMP][WI-548] p-flow GUI Testing using tours

---
+++ nh_etake_list_theme/static/src/js/referral_nurse_able_to_create_referral.js
@@ -0,0 +1,59 @@
+(function () {
+    'use strict';
+ 
+    var _t = openerp._t;
+ 
+ 
+    openerp.Tour.register({
+        id: 'referral_nurse_able_to_create_referral',
+        name: _t("Reconcile the demo bank statement"),
+        path: '/web?debug=',
+        //mode: 'test',
+ 
+
+        steps: [
+ 
+            {
+                title:     _t("Login page"),
+                element:   '.oe_topbar_name'
+            },
+ 
+            {
+                title:     _t("Referral form"),
+                element:   '.oe_menu_text:contains("Referral Forms")'
+            },
+ 
+             {
+                title:     _t("Create Referral"),
+                element:   '.oe_button.oe_list_add.oe_highlight:contains("Create")'
+            },
+ 
+            {
+                title:     _t("Select Patient"),
+                element:   '#oe-field-input-16',
+                sampleText: 'Klocko, Lindell'
+            },
+            {
+                title:     _t("Enter Symptoms"),
+                element:   'textarea[name=symptoms_notes]',
+                sampleText: 'Test Symptom Notes'
+            },
+            {
+                title:     _t("Enter Medical History"),
+                element:   'textarea[name=medical_history_notes]',
+                sampleText: 'Test Medical History Notes'
+            },
+            {
+                title:     _t("Enter Allergies"),
+                element:   'textarea[name=allergies]',
+                sampleText: 'Test Allergies'
+            },
+            {
+                title:     _t("Save"),
+                element:   '.oe_form_button_save'
+            }
+ 
+        ]
+    });
+ 
+}());
