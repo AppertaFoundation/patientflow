4f4ff3f6110700b5e4ad7630b18db17ba2c5a424
amipatel <amipatel@neovahealth.co.uk>
2015-05-11 16:21:59 +0100

    [IMP][WI-548] p-flow GUI Testing using tours

---
+++ nh_etake_list_theme/static/src/js/receptionist_able_to_update_patient_arrival.js
@@ -0,0 +1,40 @@
+/**
+ * Created by neova on 11/05/15.
+ */
+(function () {
+    'use strict';
+
+    var _t = openerp._t;
+
+
+    openerp.Tour.register({
+        id: 'receptionist_able_to_update_patient_arrival',
+        name: _t("Reconcile the demo bank statement"),
+        path: '/web?debug=',
+        //mode: 'test',
+
+
+        steps: [
+
+            {
+                title:     _t("Login page Loaded"),
+                element:   '.oe_topbar_name'
+            },
+
+            {
+                title:     _t("Referral Board Loaded"),
+                element:   '.oe_menu_text:contains("Referral Board")'
+            },
+
+            {
+                title:     _t("Notify patient arrival (click on'Arrived'  button)"),
+                element:   'td.oe_kanban_column:nth-child(2) .oe_fold_column.oe_kanban_record:first() button'
+            },
+            {
+                title:     _t("patient arrival updated and patient is in To be clerked stage"),
+                waitFor:   ('td.oe_kanban_column:nth-child(3) div.oe_fold_column.oe_kanban_record').length + 1
+            }
+        ]
+    });
+
+}());
--- nh_etake_list_theme/tests/test_referral_form_ui.py
+++ nh_etake_list_theme/tests/test_referral_form_ui.py
@@ -14,6 +14,11 @@ class TestReferralFormUI(openerp.tests.HttpCase):
             "openerp.Tour.run('referral_nurse_able_to_update_patient_arrival', 'test')",
             "openerp.Tour.tours.referral_nurse_able_to_update_patient_arrival", login="taylor"
         )
+    def test_(self):
+        self.phantom_js("/web",
+            "openerp.Tour.run('receptionist_able_to_update_patient_arrival', 'test')",
+            "openerp.Tour.tours.receptionist_able_to_update_patient_arrival", login="robert"
+        )
 
 
 
