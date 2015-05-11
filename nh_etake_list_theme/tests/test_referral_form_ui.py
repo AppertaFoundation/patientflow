7b899b713806ff641a3af5ae460edd20d7b6ab7a
amipatel <amipatel@neovahealth.co.uk>
2015-05-11 16:05:56 +0100

    [IMP][WI-548] p-flow GUI Testing using tours

--- nh_etake_list_theme/tests/test_referral_form_ui.py
+++ nh_etake_list_theme/tests/test_referral_form_ui.py
@@ -4,10 +4,15 @@ import openerp.tests
 
 class TestReferralFormUI(openerp.tests.HttpCase):
 
-    def test_referral_form_creation(self):
+    def test_referral_nurse_able_to_create_referral(self):
         self.phantom_js("/web",
             "openerp.Tour.run('referral_nurse_able_to_create_referral', 'test')",
             "openerp.Tour.tours.referral_nurse_able_to_create_referral", login="taylor"
+        )
+    def test_referral_nurse_able_to_update_patient_arrival(self):
+        self.phantom_js("/web",
+            "openerp.Tour.run('referral_nurse_able_to_update_patient_arrival', 'test')",
+            "openerp.Tour.tours.referral_nurse_able_to_update_patient_arrival", login="taylor"
         )
 
 
